03b1fc4e39b65e4112d93673571a1f615d8043c7
Gabriel Watanabe Pereira <102392345+gabrielwatanabe043@users.noreply.github.com>
2023-09-25 20:42:20 -0300

    Add files via upload

---
+++ 20230925232409_add-migration inicio.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace APIExemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmigrationinicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    idVeiculo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(type: "longtext", nullable: false),
+                    ano = table.Column<int>(type: "int", nullable: false),
+                    placa = table.Column<string>(type: "longtext", nullable: false),
+                    modelo = table.Column<string>(type: "longtext", nullable: false),
+                    marca = table.Column<string>(type: "longtext", nullable: false),
+                    cor = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.idVeiculo);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
---
+++ VeiculoController.cs
@@ -0,0 +1,57 @@
+﻿using APIExemplo.Data;
+using APIExemplo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+
+        private readonly ExemploContext _context;
+
+        public VeiculoController(ExemploContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Veiculo>>> getVeiculos() {
+            if (_context.Veiculos == null) {
+                return NotFound();
+            }
+            return await _context.Veiculos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Veiculo>> getVeiculoId(int id) {
+
+            if (_context.Veiculos == null) {
+                return NotFound();
+            }
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null) {
+                return NotFound();
+            }
+
+            return veiculo;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Veiculo>> postVeiculo(Veiculo veiculo) {
+
+            if (_context.Veiculos == null) {
+                return BadRequest();
+            }
+            if (veiculo.ano < 1930) {
+                return Problem("ERRO ANO");
+            }
+            _context.Veiculos.Add(veiculo);
+            await _context.SaveChangesAsync();
+            return veiculo;
+        }
+    }
+}
